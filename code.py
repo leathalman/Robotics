bb010f840f4f0f15e4dab4e47b63902ce3bd4231
Harrison Leath <hleath@me.com>
2020-10-22 15:14:43 -0400

    Update code.opy

--- code.py
+++ code.py
@@ -1,45 +1,30 @@
-#disclaimer, I haven't seen the robot in action yet, so I *think*
-#thats what FORWARD and REVERSE should look like, but I don't actually know - HL
-
-#def forward():
-    #left_motor.spin(FORWARD)
-    #right_motor.spin(FORWARD)
-
-#def backward():
-    #left_motor.spin(REVERSE)
-    #right_motor.spin(REVERSE)
-
-#activate the left and back motors to work in the same direction
-#activate the right motor to balance the motion in the opposite direction
-def move_left():
-    left_motor.spin(FORWARD)
-    right_motor.spin(REVERSE)
-    back_motor.spin(REVERSE)
-
-#activate the right and back motors to work in the same direction
-#activate the left motor to balance the motion in the opposite direction
-#def move_right():
-    #left_motor(REVERSE)
-    #right_motor(FORWARD)
-    #back_motor(FORWARD)
-
 #"turn left" is really more like backup in the left direction, need to observe action of robot
 #def turn_left():
-    #left_motor.spin(REVERSE)
-    #back_motor.spin(REVERSE)
-    #right_motor.spin(FORWARD)
+    #left_motor.spin(REVERSE, 100, PERCENT)
+    #back_motor.spin(REVERSE, 100, PERCENT)
+    #right_motor.spin(FORWARD, 100, PERCENT)
     
 #"turn right" is really more like backup in the right direction, need to observe action of robot
 #def turn_right():
-    #left_motor.spin(FORWARD)
-    #right_motor.spin(REVERSE)
-    #back_motor.spin(FORWARD)
+    #left_motor.spin(FORWARD, 100, PERCENT)
+    #right_motor.spin(REVERSE, 100, PERCENT)
+    #back_motor.spin(FORWARD, 100, PERCENT)
 
 while True:
-    
+        axis1Value = controller_1.axis1.position();
         axis3Value = controller_1.axis3.position();
         axis4Value = controller_1.axis4.position();
 
-        left_motor.spin(FORWARD, (axis3Value + axis4Value * 0.666), PERCENT)
-        right_motor.spin(FORWARD, (axis3Value + axis4Value * -0.666), PERCENT)
-        back_motor.spin(REVERSE, (axis4Value), PERCENT)
+        if axis1Value < -5 :
+            left_motor.spin(REVERSE, axis1Value * -1, PERCENT)
+            back_motor.spin(REVERSE, axis1Value  * -1, PERCENT)
+            right_motor.spin(FORWARD, axis1Value  * -1, PERCENT)
+        elif axis1Value > 5 :
+            left_motor.spin(FORWARD, axis1Value, PERCENT)
+            right_motor.spin(REVERSE, axis1Value, PERCENT)
+            back_motor.spin(FORWARD, axis1Value, PERCENT)
+        else :    
+            left_motor.spin(FORWARD, (axis3Value + axis4Value * 0.666), PERCENT)
+            right_motor.spin(FORWARD, (axis3Value + axis4Value * -0.666), PERCENT)
+            back_motor.spin(REVERSE, (axis4Value), PERCENT)
+            
