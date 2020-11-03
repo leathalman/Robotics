cf8b877707667b50606a5c6837cb6191b086893b
Harrison Leath <hleath@me.com>
2020-11-03 16:00:38 -0500

    Updated with grabbing functions

--- code.py
+++ code.py
@@ -1,45 +1,54 @@
-#disclaimer, I haven't seen the robot in action yet, so I *think*
-#thats what FORWARD and REVERSE should look like, but I don't actually know - HL
+#included components
+#   left_motor set to "normal" on port 2
+#   right_motor set to "normal" on port 11
+#   back_motor set to "normal" on port 1
+#   left_grab_motor set to "normal" on port 4
+#   right_grab_motor set to "normal" on port 12
 
-#def forward():
-    #left_motor.spin(FORWARD)
-    #right_motor.spin(FORWARD)
-
-#def backward():
-    #left_motor.spin(REVERSE)
-    #right_motor.spin(REVERSE)
-
-#activate the left and back motors to work in the same direction
-#activate the right motor to balance the motion in the opposite direction
-def move_left():
-    left_motor.spin(FORWARD)
-    right_motor.spin(REVERSE)
-    back_motor.spin(REVERSE)
-
-#activate the right and back motors to work in the same direction
-#activate the left motor to balance the motion in the opposite direction
-#def move_right():
-    #left_motor(REVERSE)
-    #right_motor(FORWARD)
-    #back_motor(FORWARD)
-
-#"turn left" is really more like backup in the left direction, need to observe action of robot
-#def turn_left():
-    #left_motor.spin(REVERSE)
-    #back_motor.spin(REVERSE)
-    #right_motor.spin(FORWARD)
-    
-#"turn right" is really more like backup in the right direction, need to observe action of robot
-#def turn_right():
-    #left_motor.spin(FORWARD)
-    #right_motor.spin(REVERSE)
-    #back_motor.spin(FORWARD)
+#All directions dictated with brain screen facing viewer (behind robot)
 
 while True:
-    
-        axis3Value = controller_1.axis3.position();
-        axis4Value = controller_1.axis4.position();
 
+    #represents the right chain
+    if controller_1.buttonR1.pressing() :
+        right_grab_motor.spin(FORWARD, 75, PERCENT)
+    elif controller_1.buttonR2.pressing() :
+        right_grab_motor.spin(REVERSE, 75, PERCENT)
+    else :
+        right_grab_motor.stop()
+
+    #represents the left grabber
+    if controller_1.buttonL1.pressing() :
+        left_grab_motor.spin(FORWARD, 100, PERCENT)
+    elif controller_1.buttonL2.pressing() :
+        left_grab_motor.spin(REVERSE, 100, PERCENT)
+    else : 
+        left_grab_motor.stop()
+
+    #axis 1, right joystick on x-axis
+    #axis 3, left joystick on y-axis
+    #axis 4, left joystick on x-axis
+    axis1Value = controller_1.axis1.position()
+    axis3Value = controller_1.axis3.position()
+    axis4Value = controller_1.axis4.position()
+
+    #axis 1 in negative x direction (pushed to left side)
+    #sensitivity deadzone for values 1...5 
+    if axis1Value < -5 :
+        left_motor.spin(REVERSE, axis1Value * -1, PERCENT)
+        back_motor.spin(REVERSE, axis1Value  * -1, PERCENT)
+        right_motor.spin(REVERSE, axis1Value  * -1, PERCENT)
+
+    #axis 1 in positive x direction (pushed to right side)
+    #sensitivity deadzone for values 1...5 
+    elif axis1Value > 5 :
+        left_motor.spin(FORWARD, axis1Value, PERCENT)
+        right_motor.spin(FORWARD, axis1Value, PERCENT)
+        back_motor.spin(FORWARD, axis1Value, PERCENT)
+
+    #add values for axis 3 and axis 4 and multi by approx. .666
+    #only move back motor when axis 4 is moved
+    else :    
         left_motor.spin(FORWARD, (axis3Value + axis4Value * 0.666), PERCENT)
-        right_motor.spin(FORWARD, (axis3Value + axis4Value * -0.666), PERCENT)
-        back_motor.spin(REVERSE, (axis4Value), PERCENT)
+        right_motor.spin(REVERSE, (axis3Value + axis4Value * -0.666), PERCENT)
+        back_motor.spin(REVERSE, (axis4Value), PERCENT)  
